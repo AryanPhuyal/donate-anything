dadb5287859702722c2bd3feaab4ec5b1be27154
Aryan Phuyal <phuyalrn2@gmail.com>
2020-08-03 18:23:19 +0545

    profile

--- route/auth.js
+++ route/auth.js
@@ -12,8 +12,8 @@ const { login, signup } = require("../controller/auth");
 router.post("/login", loginValidation, login);
 router.post("/signup", signUpValidation, signup);
 router.post("/signup-business", signUpValidationBusiness, signup);
-router.get("/api/me", me);
-router.put("/api/me", signUpValidation, updateProfile);
-router.put("/api/change-password", changePassword);
+router.get("/me", me);
+router.put("/me", signUpValidation, updateProfile);
+router.put("/change-password", changePassword);
 
 module.exports = router;
